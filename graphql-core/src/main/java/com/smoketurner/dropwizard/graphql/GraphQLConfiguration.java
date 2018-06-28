0b776039e4ae56ff42f2448b7deca5b685e29785
Justin Plock <jplock@smoketurner.com>
2018-06-28 14:06:59 -0400

    Use dropwizard-pom

--- graphql-core/src/main/java/com/smoketurner/dropwizard/graphql/GraphQLConfiguration.java
+++ graphql-core/src/main/java/com/smoketurner/dropwizard/graphql/GraphQLConfiguration.java
@@ -1,11 +1,11 @@
-/**
- * Copyright 2018 Smoke Turner, LLC.
+/*
+ * Copyright © 2018 Smoke Turner, LLC (contact@smoketurner.com)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -19,5 +19,5 @@ import io.dropwizard.Configuration;
 
 @FunctionalInterface
 public interface GraphQLConfiguration<C extends Configuration> {
-    GraphQLFactory getGraphQLFactory(C configuration);
+  GraphQLFactory getGraphQLFactory(C configuration);
 }
--- graphql-example/src/main/java/com/example/helloworld/HelloWorldConfiguration.java
+++ graphql-example/src/main/java/com/example/helloworld/HelloWorldConfiguration.java
@@ -1,11 +1,11 @@
-/**
- * Copyright 2018 Smoke Turner, LLC.
+/*
+ * Copyright © 2018 Smoke Turner, LLC (contact@smoketurner.com)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -15,47 +15,43 @@
  */
 package com.example.helloworld;
 
-import javax.validation.Valid;
-import javax.validation.constraints.NotNull;
-import org.hibernate.validator.constraints.NotEmpty;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.smoketurner.dropwizard.graphql.GraphQLFactory;
 import io.dropwizard.Configuration;
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import org.hibernate.validator.constraints.NotEmpty;
 
 public class HelloWorldConfiguration extends Configuration {
 
-    @NotEmpty
-    private String template = "Hello, %s!";
+  @NotEmpty private String template = "Hello, %s!";
 
-    @NotEmpty
-    private String defaultName = "Stranger";
+  @NotEmpty private String defaultName = "Stranger";
 
-    @NotNull
-    @Valid
-    public final GraphQLFactory graphql = new GraphQLFactory();
+  @NotNull @Valid public final GraphQLFactory graphql = new GraphQLFactory();
 
-    @JsonProperty
-    public String getTemplate() {
-        return template;
-    }
+  @JsonProperty
+  public String getTemplate() {
+    return template;
+  }
 
-    @JsonProperty
-    public void setTemplate(String template) {
-        this.template = template;
-    }
+  @JsonProperty
+  public void setTemplate(String template) {
+    this.template = template;
+  }
 
-    @JsonProperty
-    public String getDefaultName() {
-        return defaultName;
-    }
+  @JsonProperty
+  public String getDefaultName() {
+    return defaultName;
+  }
 
-    @JsonProperty
-    public void setDefaultName(String name) {
-        this.defaultName = name;
-    }
+  @JsonProperty
+  public void setDefaultName(String name) {
+    this.defaultName = name;
+  }
 
-    @JsonProperty
-    public GraphQLFactory getGraphQLFactory() {
-        return graphql;
-    }
+  @JsonProperty
+  public GraphQLFactory getGraphQLFactory() {
+    return graphql;
+  }
 }
