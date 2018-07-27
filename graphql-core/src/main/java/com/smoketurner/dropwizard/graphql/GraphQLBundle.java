912bf9f34d04c00635e3b56522b1bb9f932dc394
Justin Plock <jplock@smoketurner.com>
2018-07-26 20:53:00 -0400

    Upgrade to graphql-java-servlet 6.1.2

--- graphql-core/src/main/java/com/smoketurner/dropwizard/graphql/GraphQLBundle.java
+++ graphql-core/src/main/java/com/smoketurner/dropwizard/graphql/GraphQLBundle.java
@@ -1,23 +1,21 @@
 /*
  * Copyright © 2018 Smoke Turner, LLC (contact@smoketurner.com)
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
  */
 package com.smoketurner.dropwizard.graphql;
 
 import graphql.schema.GraphQLSchema;
-import graphql.servlet.GraphQLServlet;
-import graphql.servlet.SimpleGraphQLServlet;
+import graphql.servlet.GraphQLQueryInvoker;
+import graphql.servlet.SimpleGraphQLHttpServlet;
 import io.dropwizard.Configuration;
 import io.dropwizard.ConfiguredBundle;
 import io.dropwizard.assets.AssetsBundle;
@@ -37,10 +35,12 @@ public abstract class GraphQLBundle<C extends Configuration>
     final GraphQLFactory factory = getGraphQLFactory(configuration);
 
     final GraphQLSchema schema = factory.build();
-    final GraphQLServlet servlet =
-        SimpleGraphQLServlet.builder(schema)
-            .withInstrumentation(factory.getInstrumentations())
-            .build();
+
+    final GraphQLQueryInvoker queryInvoker =
+        GraphQLQueryInvoker.newBuilder().withInstrumentation(factory.getInstrumentations()).build();
+
+    final SimpleGraphQLHttpServlet servlet =
+        SimpleGraphQLHttpServlet.newBuilder(schema).withQueryInvoker(queryInvoker).build();
 
     environment.servlets().addServlet("graphql", servlet).addMapping("/graphql", "/schema.json");
   }
