5244c93a4daf408afc127b2a651cf17315006f35
Justin Plock <jplock@smoketurner.com>
2018-11-21 08:59:27 -0500

    Reformat

--- graphql-core/src/main/java/com/smoketurner/dropwizard/graphql/GraphQLValidationError.java
+++ graphql-core/src/main/java/com/smoketurner/dropwizard/graphql/GraphQLValidationError.java
@@ -15,12 +15,12 @@
  */
 package com.smoketurner.dropwizard.graphql;
 
-import java.util.List;
-import javax.annotation.Nullable;
 import graphql.ErrorType;
 import graphql.GraphQLError;
 import graphql.language.SourceLocation;
 import graphql.schema.DataFetcher;
+import java.util.List;
+import javax.annotation.Nullable;
 
 /**
  * Generic class to return a validation error message to the user from a {@link DataFetcher} without
