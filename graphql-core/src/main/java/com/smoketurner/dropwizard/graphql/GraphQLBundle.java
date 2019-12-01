f30a4bc05f8c418b49116d539472a0506c2258bb
Justin Plock <jplock@smoketurner.com>
2019-12-01 16:01:15 -0500

    Updates after updating to GraphQL servlet 9.0.0

--- graphql-core/src/main/java/com/smoketurner/dropwizard/graphql/GraphQLBundle.java
+++ graphql-core/src/main/java/com/smoketurner/dropwizard/graphql/GraphQLBundle.java
@@ -16,9 +16,9 @@
 package com.smoketurner.dropwizard.graphql;
 
 import graphql.execution.preparsed.PreparsedDocumentProvider;
+import graphql.kickstart.execution.GraphQLQueryInvoker;
 import graphql.schema.GraphQLSchema;
 import graphql.servlet.GraphQLHttpServlet;
-import graphql.servlet.core.GraphQLQueryInvoker;
 import io.dropwizard.Configuration;
 import io.dropwizard.ConfiguredBundle;
 import io.dropwizard.assets.AssetsBundle;
@@ -48,8 +48,8 @@ public abstract class GraphQLBundle<C extends Configuration>
             .withInstrumentation(factory.getInstrumentations())
             .build();
 
-    final graphql.servlet.config.GraphQLConfiguration config =
-        graphql.servlet.config.GraphQLConfiguration.with(schema).with(queryInvoker).build();
+    final graphql.servlet.GraphQLConfiguration config =
+        graphql.servlet.GraphQLConfiguration.with(schema).with(queryInvoker).build();
 
     final GraphQLHttpServlet servlet = GraphQLHttpServlet.with(config);
 
