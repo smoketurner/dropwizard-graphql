31aa9e5469d2722c1cd487888abc7f4e1114de55
Justin Plock <jplock@smoketurner.com>
2023-04-02 10:38:25 -0400

    Fix Spotbug error

--- graphql-core/src/main/java/com/smoketurner/dropwizard/graphql/GraphQLFactory.java
+++ graphql-core/src/main/java/com/smoketurner/dropwizard/graphql/GraphQLFactory.java
@@ -39,6 +39,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
@@ -70,7 +71,7 @@ public class GraphQLFactory {
 
   @JsonProperty
   public List<String> getSchemaFiles() {
-    return schemaFiles;
+    return Collections.unmodifiableList(schemaFiles);
   }
 
   @JsonProperty
